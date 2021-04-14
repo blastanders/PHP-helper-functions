7ed52784cadd10674763c37d6d01e4d651df72ac
AZ <aaron@bonntech.com.au>
2021-04-14 16:35:32 +1000

    Created pre_var_dump(). A nicer var_dump with extra useful info :)

---
+++ pre_var_dump.php
@@ -0,0 +1,32 @@
+<?php 
+function pre_var_dump() {
+    $back_trace = debug_backtrace();
+    $last = end($back_trace);
+    
+    $back_trace_array = array();
+    
+    foreach($back_trace as $each_trace) {
+        $back_trace_array[] = @$each_trace['file'] . ' ' . @$each_trace['line'];
+    }
+    
+    do {
+        $color = substr(md5(rand()), 0, 6);
+        $color_avg = (hexdec(substr($color, 0, 2)) + hexdec(substr($color, 2, 2)) + hexdec(substr($color, 4, 2)))/3;
+    
+    } while ($color_avg < 190);//keep generating bg color until got a bg color that is white enough
+    
+    echo "<pre style='background-color:#{$color}'>";
+    
+    $step = 0;
+    
+    foreach($back_trace_array as $each_trace) {
+        echo PHP_EOL . "[{$step}][{$each_trace}]";
+        $step++;
+    }
+    echo PHP_EOL;
+    
+    foreach (func_get_args() as $param) {
+        var_dump($param);
+    }
+    echo "</pre>";
+}
